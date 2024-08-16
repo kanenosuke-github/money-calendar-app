8751dd800eb3c7a1e3482c591d858095cf480bde
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-08-16 20:17:42 +0900

    schedule-controller0816

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
-    //
+    public function index()
+    {
+        $schedules = Schedule::all();
+        return view('schedules.index',compact('schedules'));
+    }
+
+    public function create()
+    {
+        return view('schedules.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'date' => 'required|date',
+            'schedule' => 'required|string|max:255',
+            'location' => 'nullable|string|max:255',
+            'time' => 'nullable|date_format:H:i',
+            'memo' => 'nullable|string',
+        ]);
+
+        Schedule::create($validated);
+        return redirect()->route('schedules.index');
+    }
 }
