86429a5b92692331e703c6ffc3927f90d7fd2cc2
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-08-16 20:43:25 +0900

    householdaccount-controller0816

--- app/Http/Controllers/HouseholdAccountController.php
+++ app/Http/Controllers/HouseholdAccountController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class HouseholdAccountController extends Controller
 {
-    //
+    public function index()
+    {
+        $householdAccounts = HouseholdAccount::all();
+        return view('household-accounts.index',compact('household-accounts'));
+    }
+
+    public function create()
+    {
+        return view('household-accounts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'date' => 'required|date',
+            'amount' => 'required|integer',
+            'item' => 'nullabll|string|max:255',
+            'category' => 'required|string|max:255',
+            'memo' => 'nullable|string',
+        ]);
+
+        HouseholdAccount::create($validated);
+        return redirect()->route('household-accounts.index')->with('success','家計簿が追加されました');
+    }
 }
