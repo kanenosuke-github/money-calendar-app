ada07f63dbc68b8cde29fdcf44d78d69ec643184
kanenosuke-github <shun.216.nosuke@gmail.com>
2024-08-22 09:45:17 +0900

    view0822

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -1,11 +1,20 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header></header>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+<h1>{{ \Carbon\Carbon::parse($month)->format('Y年m月') }}</h1>
+<a href="{{ route('calendar.index', ['month' => \Carbon\Carbon::parse($month)->subMonth()->format('Y-m')]) }}">前月</a>
+<a href="{{ route('calendar.index', ['month' => \Carbon\Carbon::parse($month)->addMonth()->format('Y-m')]) }}">次月</a>
+
+<table>
+    <!-- カレンダーのヘッダー（日付）を表示 -->
+    <tr>
+        @for ($day = 1; $day <= \Carbon\Carbon::parse($month)->daysInMonth; $day++)
+            <td>
+                <a href="{{ route('schedules.create', ['date' => $month . '-' . $day]) }}">
+                    {{ $day }}
+                </a>
+            </td>
+        @endfor
+    </tr>
+</table>
+@endsection
